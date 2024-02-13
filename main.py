0fc2c4ce163c5ba61ad33e839a029b5046f74f6b
houdadevpy <157724054+houdadevpy@users.noreply.github.com>
2024-02-13 14:29:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,20 @@ def check_fun():
     return True
   else:
     return False
+
+
+# unit test case 
+import unittest 
+
+class TestStringMethods(unittest.TestCase): 
+	# test function to test equality of two value 
+	def test_positive(self): 
+		firstValue = "geeks"
+		secondValue = "geeks"
+		# error message in case if test case got failed 
+		message = "First value and second value are not equal !"
+		# assertEqual() to check equality of first & second value 
+		self.assertEqual(firstValue, secondValue, message) 
+
+if __name__ == '__main__': 
+	unittest.main() 
